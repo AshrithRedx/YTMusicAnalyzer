a2f92863c66e7a0f1aa2301a07fecebbf2573fda
AshrithRedx <ashrithreddy125@gmail.com>
2025-04-19 20:04:56 +0530

    Added CORS support

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify, send_file
 from fetch_playlist import get_playlist_data
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
 
 app = Flask(__name__)
 
