1149dc74921c2c9c2993b70872db13d6e17d9694
AshrithRedx <ashrithreddy125@gmail.com>
2025-04-19 23:44:15 +0530

    Fixed CORS for vercel

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_file
 from fetch_playlist import get_playlist_data
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://yt-music-analyzer.vercel.app"])
 
 
 app = Flask(__name__)
